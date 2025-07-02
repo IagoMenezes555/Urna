d6420296c5b77618542b9883d5dab62f93fb264c
Iago <iagomenezes555@gmail.com>
2025-07-02 12:15:58 -0300

    feat: pad

--- src/app/components/pad/pad.component.ts
+++ src/app/components/pad/pad.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pad',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pad.component.html',
   styleUrl: './pad.component.scss'
 })
 export class PadComponent {
 
+  numbers = [
+    9,8,7,6,5,4,3,2,1,0
+  ]
 }
