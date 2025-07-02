792ca3bd9669c701204512cc6fe2ef402642255f
Iago <iagomenezes555@gmail.com>
2025-07-02 11:05:49 -0300

    urna

---
+++ src/app/components/pad/pad.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pad',
+  imports: [],
+  templateUrl: './pad.component.html',
+  styleUrl: './pad.component.scss'
+})
+export class PadComponent {
+
+}
---
+++ src/app/components/urna/urna.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-urna',
+  imports: [],
+  templateUrl: './urna.component.html',
+  styleUrl: './urna.component.scss'
+})
+export class UrnaComponent {
+
+}
